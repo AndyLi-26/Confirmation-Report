89c6f80ee495e010c0a03bc21c2aa1bb437cfad2
andy li <1953812182@qq.com>
2025-04-16 12:49:00 +1000

    better structure, and added RQ and RO

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Delete common LaTeX auxiliary files
+rm -f *.aux *.log *.toc *.lof *.lot *.out *.bbl *.blg *.synctex.gz *.fdb_latexmk *.fls *.nav *.snm *.vrb *.pdf
+echo "LaTeX cache files deleted."
